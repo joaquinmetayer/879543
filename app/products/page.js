081ebdb53001f5a84b187e846252be7b1f9c6bfd
Joaquin Metayer <joaquinmetayer@gmail.com>
2024-01-01 07:02:26 -0300

    36

--- app/products/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="w-full h-screen flex justify-center aling-center">
-      <p className="font-bold text-5xl p-10 title">CLOSEFRIENDS</p>
-    </div>
-  );
-}
