b0762e322b407a583523d4bdf6418a4232bcc86a
Joaquin Metayer <joaquinmetayer@gmail.com>
2024-02-11 10:28:14 -0300

    40

---
+++ src/app/fxblue/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useEffect } from "react";
+export default function Home() {
+  useEffect(() => {
+    window.location.href =
+      "https://linktw.in/GtYkiO";
+  }, []);
+  return (
+    <div className="bg-black h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter text-white text-xl font-bold">
+      ABRIENDO INSTAGRAM...
+    </div>
+  );
+}
---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useEffect } from "react";
+export default function Home() {
+  useEffect(() => {
+    window.location.href =
+      "https://linktw.in/AiQsvR";
+  }, []);
+  return (
+    <div className="bg-black h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter text-white text-xl font-bold">
+      ABRIENDO YOUTUBE...
+    </div>
+  );
+}
